3bbe61dbfe2313e956ffdada7a0290c9cca8d815
FX <danielfx011@gmail.com>
2024-08-22 12:53:06 +0100

    update

--- plugins/download.js
+++ plugins/download.js
@@ -1,29 +1,33 @@
-const { bot, mode, isUrl, getBuffer, getJson, validateQuality } = require('../lib')
+const { bot, mode } = require('../lib')
 const { Facebook, Instagram, Twitter, Tiktok } = require('../lib/Misc')
-const { ytsdl } = require('../lib/ytdl')
+const { isTwiiterUrl, isFacebookUrl, isInstagramUrl, isLinkedInUrl, isYouTubeUrl, isTikTokUrl } = require('./Misc/vaildator')
 bot(
  {
   pattern: 'facebook',
+  fromMe: mode,
   info: 'Download Facebook Media',
   type: 'download',
  },
  async (message, match) => {
   if (!match) return await message.sendReply('_Provide Facebook link!_')
+  if (!isFacebookUrl) return await message.sendReply('_invaild facebook url_')
   await message.sendReply('processing')
   const facebook = new Facebook(match)
   const hdVideo = await facebook.getHdVideo()
- return await message.send(hdVideo, {}, 'video')
+  return await message.send(hdVideo, {}, 'video')
  }
 )
 
 bot(
  {
   pattern: 'instagram',
+  fromMe: mode,
   info: 'Download Instagram Media',
   type: 'download',
  },
  async (message, match) => {
   if (!match) return await message.sendReply('_Provide Instagram Link!_')
+  if (!isInstagramUrl) return await message.sendReply('_invaild instagram url_')
   await message.sendReply('_Downloading_')
   const insta = new Instagram()
   const result = await insta.download(match)
@@ -34,11 +38,13 @@ bot(
 bot(
  {
   pattern: 'twitter',
+  fromMe: mode,
   info: 'Downloads twitter media',
   type: 'download',
  },
  async (message, match) => {
   if (!match) return await message.sendReply('_provide x url_')
+  if (!isTwiiterUrl) return await message.sendReply('_invaild twiiter url_')
   await message.sendReply('_Downloading_')
   const twitter = new Twitter()
   const file = await twitter.download(match)
@@ -49,123 +55,16 @@ bot(
 bot(
  {
   pattern: 'tiktok',
+  fromMe: mode,
   info: 'Downloads Tiktok Media',
   type: 'download',
  },
  async (message, match) => {
   if (!match) return message.sendReply('_provide tiktok url_')
+  if (!isTikTokUrl) return await message.sendReply('_invaild tiktok url_')
   await message.sendReply('_downloading_')
   const tiktok = new Tiktok()
   const file = tiktok.download(match)
   return await message.send(file, {}, 'video')
- }
-)
-
-bot(
- {
-  pattern: 'yta',
-  fromMe: mode,
-  desc: 'Download audio from youtube',
-  type: 'download',
- },
- async (message, match) => {
-  match = match || message.reply_message.text
-  if (!match) return await message.sendReply('Give me a youtube link')
-  if (!isUrl(match)) return await message.sendReply('Give me a youtube link')
-  let { dlink, title } = (await getJson(`https://api.thexapi.xyz/api/v1/download/youtube/audio?url=${match}`)).data
-  await message.sendReply(`_Downloading ${title}_`)
-  let buff = await getBuffer(dlink)
-  return await message.sendMessage(
-   message.jid,
-   buff,
-   {
-    mimetype: 'audio/mpeg',
-    filename: title + '.mp3',
-   },
-   'audio'
-  )
- }
-)
-
-bot(
- {
-  pattern: 'ytv',
-  fromMe: mode,
-  desc: 'Download audio from youtube',
-  type: 'download',
- },
- async (message, match) => {
-  match = match || message.reply_message.text
-  let url = getUrl(match)[0]
-  if (!url) return await message.sendReply('Give me a youtube link\n\nExample: ytv youtube.com/watch?v=xxxxx 480p')
-  let quality = match.split(';')[1]
-  if (quality && !validateQuality(quality)) {
-   return await message.sendReply('Invalid Resolution \nSupported: 144p, 240p, 360p, 480p, 720p, 1080p, 1440p, 2160p')
-  } else if (!quality) quality = '360p'
-  if (!match) return await message.sendReply('Give me a youtube link\n\nExample: ytv youtube.com/watch?v=xxxxx 480p')
-  if (!isUrl(match)) return await message.sendReply('Give me a youtube link\n\nExample: ytv youtube.com/watch?v=xxxxx 480p')
-  let requrl = `https://api.thexapi.xyz/api/v1/download/youtube/video?url=${url}&quality=${quality}`
-  let response = (await getJson(requrl)).data
-  const { dlink, title } = response
-  console.log(response)
-  await message.sendReply(`_Downloading ${title}_`)
-  return await message.sendMessage(
-   message.jid,
-   dlink,
-   {
-    mimetype: 'video/mp4',
-    filename: title + '.mp4',
-   },
-   'video'
-  )
- }
-)
-
-bot(
- {
-  pattern: 'song',
-  fromMe: mode,
-  desc: 'Download audio from youtube',
-  type: 'download',
- },
- async (message, match) => {
-  match = match || message.reply_message.text
-  if (!match) return await message.sendReply('Give me a query')
-  let { dlink, title } = await ytsdl(match)
-  await message.sendReply(`_Downloading ${title}_`)
-  let buff = await getBuffer(dlink)
-  return await message.sendMessage(
-   message.jid,
-   buff,
-   {
-    mimetype: 'audio/mpeg',
-    filename: title + '.mp3',
-   },
-   'audio'
-  )
- }
-)
-
-bot(
- {
-  pattern: 'video',
-  fromMe: mode,
-  desc: 'Download video from youtube',
-  type: 'download',
- },
- async (message, match) => {
-  match = match || message.reply_message.text
-  if (!match) return await message.sendReply('Give me a query')
-  let { dlink, title } = await ytsdl(match, 'video')
-  await message.sendReply(`_Downloading ${title}_`)
-  return await message.sendMessage(
-   message.jid,
-   dlink,
-   {
-    mimetype: 'video/mp4',
-    filename: title + '.mp4',
-   },
-   'video'
-  )
  }
 )
--- plugins/misc.js
+++ plugins/misc.js
@@ -1,9 +1,4 @@
 const { bot, qrcode, Bitly, mode, isUrl, readQr } = require('../lib/')
-const { getLyrics } = require('../lib/functions')
-const { fromBuffer } = require('file-type')
-const { ffmpeg, parseTimeToSeconds } = require('../lib/functions')
-const { removeBg } = require('../lib/functions')
-const config = require('../config')
 const { server } = require('../lib/Misc')
 
 bot(
@@ -65,79 +60,3 @@ bot(
  }
 )
 
-bot(
- {
-  pattern: 'lyric',
-  fromMe: mode,
-  desc: 'Searches for lyrics based on the format: song;artist',
-  type: 'misc',
- },
- async (message, match) => {
-  const [song, artist] = match.split(';').map(item => item.trim())
-  if (!song || !artist) {
-   await message.sendReply('Search with this format: \n\t_lyric song;artist_')
-  } else {
-   try {
-    const data = await getLyrics(song, artist)
-    if (data) {
-     return await message.sendReply(`*Artist:* ${data.artist_name}\n*Song:* ${data.song}\n*Lyrics:*\n${data.lyrics.trim()}`)
-    } else {
-     return await message.sendReply('No lyrics found for this song by this artist.')
-    }
-   } catch (error) {
-    return await message.sendReply('An error occurred while fetching lyrics.')
-   }
-  }
- }
-)
-bot(
- {
-  pattern: 'trim',
-  fromMe: mode,
-  desc: 'Trim the video or audio',
-  type: 'misc',
- },
- async (message, match, m) => {
-  if (!message.reply_message || (!message.reply_message.video && !message.reply_message.audio)) {
-   return await message.sendMessage('Reply to a media file')
-  }
-  if (!match) return await message.sendMessage('Give the start and end time in this format: mm:ss|mm:ss')
-
-  const [start, end] = match.split('|')
-  if (!start || !end) return await message.sendMessage('Give the start and end time in this format: mm:ss|mm:ss')
-  const buffer = await m.quoted.download()
-  const startSeconds = parseTimeToSeconds(start)
-  const endSeconds = parseTimeToSeconds(end)
-  const duration = endSeconds - startSeconds
-  const ext = (await fromBuffer(buffer)).ext
-  const args = ['-ss', `${startSeconds}`, '-t', `${duration}`, '-c', 'copy']
-  const trimmedBuffer = await ffmpeg(buffer, args, ext, ext)
-  message.sendFile(trimmedBuffer)
- }
-)
-
-bot(
- {
-  pattern: 'rmbg',
-  fromMe: mode,
-  desc: 'Remove background of an image',
-  type: 'image',
- },
- async (message, match, m) => {
-  if (!config.REMOVEBG) return await message.sendMessage(message.jid, 'Set RemoveBg API Key in config.js \n Get it from https://www.remove.bg/api')
-  if (!message.reply_message && !message.reply_message.image) return await message.sendReply('Reply to an image')
-  let buff = await m.quoted.download()
-  let buffer = await removeBg(buff)
-  if (!buffer) return await message.sendReply('An error occured')
-  await message.sendMessage(
-   message.jid,
-   buffer,
-   {
-    quoted: message.reply_message.key,
-    mimetype: 'image/png',
-    fileName: 'removebg.png',
-   },
-   'document'
-  )
- }
-)
