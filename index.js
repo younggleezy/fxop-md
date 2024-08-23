2f726ebcc4cfb1d6c4b521c6fe4e0856b78146d9
FX <danielfx011@gmail.com>
2024-08-23 14:34:12 +0100

    fix

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-const path = require('path')
-const config = require('./config')
+const { join } = require('path')
+const { DATABASE } = require('./config')
 const connect = require('./lib/client')
 const io = require('socket.io-client')
 const { getandRequirePlugins } = require('./lib/database/plugins')
 const { fetchFiles, createSession } = require('./lib/Misc')
-const { delay } = require('astrofx011')
+const chalk = require('chalk')
 const RunServer = require('./server')
+const { delay } = require('astrofx011')
 async function startBot() {
  try {
   const server = new RunServer(8000)
   server.start()
-  delay(3000)
   await createSession()
-  await fetchFiles(path.join(__dirname, '/lib/database/'))
-  console.log('Syncing Database')
-  await config.DATABASE.sync()
-  console.log('⬇  Installing Plugins...')
-  await fetchFiles(path.join(__dirname, '/plugins/'))
+  await fetchFiles(join(__dirname, '/lib/database/'))
+  console.log('Database Synchronized')
+  await DATABASE.sync()
+  await fetchFiles(join(__dirname, '/plugins/'))
   await getandRequirePlugins()
-  console.log('Plugins Installed!')
-
+  delay(3000)
   const ws = io('https://socket-counter.onrender.com/', { reconnection: true })
   ws.on('connect', () => console.log('Connected to server'))
   ws.on('disconnect', () => console.log('Disconnected from server'))
@@ -27,7 +25,7 @@ async function startBot() {
   return await connect()
  } catch (error) {
   console.error('Initialization error:', error)
-  return process.exit(1) // Exit with error status
+  return process.exit(1)
  }
 }
 
