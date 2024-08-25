1fea175c42f65f104c42ec4b0e1a2619187bbe67
FX <danielfx011@gmail.com>
2024-08-25 10:14:33 +0100

    fix

--- index.js
+++ index.js
@@ -2,34 +2,20 @@ const fs = require("fs").promises;
 const path = require("path");
 const config = require("./config");
 const connect = require("./lib/client.js");
+const {fetchFiles}=require('./lib')
 const { getandRequirePlugins } = require("./lib/database/plugins");
 const {existsSync, mkdirSync} = require('fs')
 global.__basedir = __dirname;
 
-const readAndRequireFiles = async (directory) => {
+async function runBot() {
   try {
-    const files = await fs.readdir(directory);
-    return Promise.all(
-      files
-        .filter((file) => path.extname(file).toLowerCase() === ".js")
-        .map((file) => require(path.join(directory, file)))
-    );
-  } catch (error) {
-    console.error("Error reading and requiring files:", error);
-    throw error;
-  }
-};
-
-async function initialize() {
-  console.log("fxop");
-  try {
-    await readAndRequireFiles(path.join(__dirname, "/lib/database/"));
+    await fetchFiles(path.join(__dirname, "/lib/database/"));
     console.log("Syncing Database");
 
     await config.DATABASE.sync();
 
     console.log("⬇  Installing Plugins...");
-    await readAndRequireFiles(path.join(__dirname, "/plugins/"));
+    await fetchFiles(path.join(__dirname, "/plugins/"));
     await getandRequirePlugins();
     console.log("✅ Plugins Installed!");
     return await connect();
@@ -39,4 +25,4 @@ async function initialize() {
   }
 }
 
-initialize();
+runBot();
