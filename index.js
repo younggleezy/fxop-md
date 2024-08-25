496e1d4a335cc176ed18ed0728b3db4ea1976ea6
FX <danielfx011@gmail.com>
2024-08-25 17:07:03 +0100

    fix

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-const fs = require("fs").promises;
-const path = require("path");
-const config = require("./config");
-const connect = require("./lib/client.js");
-const fetchFiles =require('./lib/modules.js')
-const { getandRequirePlugins } = require("./lib/database/plugins");
-const {existsSync, mkdirSync} = require('fs')
-global.__basedir = __dirname;
+const path = require('path')
+const config = require('./config')
+const { connect } = require('./lib/client.js')
+const fetchFiles = require('./lib/modules.js')
+const { getandRequirePlugins } = require('./lib/database/plugins')
+global.__basedir = __dirname
 
 async function runBot() {
-  try {
-    await fetchFiles(path.join(__dirname, "/lib/database/"));
-    console.log("Syncing Database");
+ try {
+  await fetchFiles(path.join(__dirname, '/lib/database/'))
+  console.log('Syncing Database')
 
-    await config.DATABASE.sync();
+  await config.DATABASE.sync()
 
-    console.log("⬇  Installing Plugins...");
-    await fetchFiles(path.join(__dirname, "/plugins/"));
-    await getandRequirePlugins();
-    console.log("✅ Plugins Installed!");
-    return await connect();
-  } catch (error) {
-    console.error("Initialization error:", error);
-    return process.exit(1);
-  }
+  console.log('⬇  Installing Plugins...')
+  await fetchFiles(path.join(__dirname, '/plugins/'))
+  await getandRequirePlugins()
+  console.log('✅ Plugins Installed!')
+  return await connect()
+ } catch (error) {
+  console.error('Initialization error:', error)
+  return process.exit(1)
+ }
 }
 
-runBot();
+runBot()
