be9e704a45328babc11e6eb5e729e48346b57854
FX <danielfx011@gmail.com>
2024-08-26 17:34:33 +0100

    fix

--- plugins/user.js
+++ plugins/user.js
@@ -1,7 +1,7 @@
 const { command, parsedJid } = require('../lib')
 const { WarnDB } = require('../lib/database')
 const { WARN_COUNT } = require('../config')
-const { saveWarn, resetWarn } = WarnDB
+const { saveWarn, resetWarn, getWarns, deleteLastWarn } = WarnDB
 
 command(
  {
@@ -19,7 +19,6 @@ command(
 
   const warnInfo = await saveWarn(userId, reason)
   let userWarnCount = warnInfo ? warnInfo.warnCount : 0
-  userWarnCount++
   await message.reply(`_User @${userId.split('@')[0]} warned._ \n_Warn Count: ${userWarnCount}._ \n_Reason: ${reason}_`, { mentions: [userId] })
   if (userWarnCount > WARN_COUNT) {
    const jid = parsedJid(userId)
@@ -44,5 +43,52 @@ command(
   return await message.reply(`_Warnings for @${userId.split('@')[0]} reset_`, {
    mentions: [userId],
   })
+ }
+)
+
+command(
+ {
+  pattern: 'delwarn',
+  fromMe: true,
+  desc: 'Delete the last warning for a user',
+  type: 'user',
+ },
+ async message => {
+  const userId = message.mention[0] || message.reply_message.jid
+  if (!userId) return message.reply('_Mention or reply to someone_')
+  const success = await deleteLastWarn(userId)
+  if (success) {
+   return await message.reply(`_Last warning for @${userId.split('@')[0]} deleted_`, {
+    mentions: [userId],
+   })
+  } else {
+   return await message.reply(`_No warnings found for @${userId.split('@')[0]}_`, {
+    mentions: [userId],
+   })
+  }
+ }
+)
+
+command(
+ {
+  pattern: 'getwarn',
+  fromMe: true,
+  desc: 'Get warnings for a user',
+  type: 'user',
+ },
+ async message => {
+  const userId = message.mention[0] || message.reply_message.jid
+  if (!userId) return message.reply('_Mention or reply to someone_')
+  const warnInfo = await getWarns(userId)
+  if (warnInfo && warnInfo.warnCount > 0) {
+   const reasons = warnInfo.reasons ? warnInfo.reasons.join('\n') : 'No reasons provided'
+   return await message.reply(`_Warnings for @${userId.split('@')[0]}:_ \n_Count: ${warnInfo.warnCount}_ \n_Reasons:_\n${reasons}`, {
+    mentions: [userId],
+   })
+  } else {
+   return await message.reply(`_No warnings found for @${userId.split('@')[0]}_`, {
+    mentions: [userId],
+   })
+  }
  }
 )
