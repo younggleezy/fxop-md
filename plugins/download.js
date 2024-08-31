20cf61eb76288edb218580fc4b2d8a1b0e0eda7c
FX <danielfx011@gmail.com>
2024-08-31 18:06:02 +0100

    fix ig

--- plugins/download.js
+++ plugins/download.js
@@ -1,39 +1,17 @@
-const { Module, IronMan, Scraper, mode } = require("../lib");
-const axios = require("axios");
+const { Module, Scraper, mode } = require("../lib");
 Module(
  {
   pattern: "insta",
-  fromMe: true,
+  fromMe: mode,
   desc: "Downlaod post or reel from Instagram",
   type: "downloader",
  },
- async (message, match, m) => {
-  if (!match || !match.includes("instagram.com")) {
-   return await message.reply("Need an Instagram URL.");
-  }
-
-  try {
-   const res = await axios.get(IronMan(`ironman/dl/v2/insta?url=${match}`));
-   const data = res.data;
-
-   if (data.status === 200 && Array.isArray(data.media) && data.media.length > 0) {
-    for (const url of data.media) {
-     if (url) {
-      if (match.includes("/p/")) {
-       await message.client.sendMessage({ image: { url } });
-      } else if (match.includes("/reel/") || match.includes("/tv/")) {
-       await message.client.sendMessage({ video: { url } });
-      } else {
-       await message.client.sendMessage({ video: { url } });
-      }
-     }
-    }
-   } else {
-    throw new Error("No media found./nCheck if your url is valid");
-   }
-  } catch (error) {
-   await message.reply("No media found./nCheck if your url is valid");
-  }
+ async (message, match) => {
+  if (!match) return await message.sendReply("_provide instagram url");
+  await message.sendReply("_downloading_");
+  const ig = new Scraper();
+  const buff = await ig.instagram(match);
+  return await message.sendFile(buff);
  }
 );
 
