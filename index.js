621ceb2f64a87cb62c3da96e882c9a0b346e3a71
FX <danielfx011@gmail.com>
2024-09-01 21:19:09 +0200

    fix

--- index.js
+++ index.js
@@ -5,40 +5,22 @@ const { connect } = require("./lib");
 const { getandRequirePlugins } = require("./lib/database/plugins");
 global.__basedir = __dirname;
 
-const parse = async directory => {
- try {
-  const files = await fs.readdir(directory);
-  const jsFiles = files.filter(file => path.extname(file).toLowerCase() === ".js");
-
-  const results = await Promise.allSettled(
-   jsFiles.map(async file => {
-    try {
-     return { file, module: require(path.join(directory, file)), error: null };
-    } catch (error) {
-     return { file, module: null, error: error.message };
-    }
-   })
-  );
-
-  results.forEach(({ status, value }) => {
-   if (status === "rejected") {
-    console.error(`Error loading file: ${value.file}\n`, value.error);
-   }
-  });
-
-  return results.filter(({ status }) => status === "fulfilled").map(({ value }) => value.module);
- } catch (error) {
-  console.error("Error reading files from directory:", error.message);
-  throw error;
- }
-};
+const parseDir = async (directory) => {
+  try {
+   const files = await fs.readdir(directory);
+   return Promise.all(files.filter((file) => path.extname(file).toLowerCase() === ".js").map((file) => require(path.join(directory, file))));
+  } catch (error) {
+   console.error("Error reading and requiring files:", error);
+   throw error;
+  }
+ };
 
 async function initialize() {
  try {
-  await parse(path.join(__dirname, "/lib/database/"));
+  await parseDir(path.join(__dirname, "/lib/database/"));
   console.log("Syncing Database");
   await config.DATABASE.sync();
-  await parse(path.join(__dirname, "/plugins/"));
+  await parseDir(path.join(__dirname, "/plugins/"));
   await getandRequirePlugins();
   console.log("External Modules Installed");
   return await connect();
