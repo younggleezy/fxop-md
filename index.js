3c65ffb209c5836e4c868617b7c5dfd276555424
FX <danielfx011@gmail.com>
2024-09-01 21:17:46 +0200

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs").promises;
 const path = require("path");
 const config = require("./config");
-const connect = require("./lib/client");
+const { connect } = require("./lib");
 const { getandRequirePlugins } = require("./lib/database/plugins");
 global.__basedir = __dirname;
 
