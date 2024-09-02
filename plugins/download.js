829f47b0941176dcd69d8f02c0565da3e0779bc1
FX <danielfx011@gmail.com>
2024-09-02 15:29:27 +0100

    fix

---
+++ plugins/download.js
@@ -0,0 +1,16 @@
+const { command, mode, ScrapeDl } = require("../lib");
+command(
+ {
+  pattern: "facebook",
+  fromMe: mode,
+  desc: "Downloads Facebook Media",
+  type: "download",
+ },
+ async (message, match) => {
+  if (!match) return message.reply("_provide vaild facebook link_");
+  await message.reply("_Downloading_");
+  const media = new ScrapeDl();
+  const buff = new media.facebook(match);
+  return message.sendFile(buff);
+ }
+);
