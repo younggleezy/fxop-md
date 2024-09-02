75fe984726402bd90d517d530dd9550545ccac85
FX <danielfx011@gmail.com>
2024-09-02 18:48:05 +0100

    fix

--- plugins/download.js
+++ plugins/download.js
@@ -9,7 +9,7 @@ command(
  async (message, match) => {
   if (!match) return message.reply("_provide vaild facebook link_");
   await message.reply("_Downloading_");
-  const media = new ScrapeDl();
+  const media = ScrapeDl();
   const buff = await media.facebook(match);
   return message.sendFile(buff);
  }
@@ -25,7 +25,7 @@ command(
  async (message, match) => {
   if (!match) return message.reply("_provide vaild instagram link_");
   await message.reply("_Downloading_");
-  const media = new ScrapeDl();
+  const media = ScrapeDl();
   const buff = await media.instagram(match);
   return message.sendFile(buff);
  }
@@ -33,12 +33,10 @@ command(
 
 command(
  {
-  pattern: "twitter", 
+  pattern: "twitter",
   fromMe: true,
-  desc: "Downloads Twitter Media", 
-  type: "download" 
-  }, 
- async (message, match) => {
-
- }
-) 
+  desc: "Downloads Twitter Media",
+  type: "download",
+ },
+ async (message, match) => {}
+);
