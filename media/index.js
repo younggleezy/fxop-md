8cfe873cf5ce124f60bdfb50e2ddb2c9746c80f7
FX <danielfx011@gmail.com>
2024-09-03 22:11:02 +0100

    Update index.js

--- media/index.js
+++ media/index.js
@@ -22,4 +22,20 @@ async function buffThumb(filePath) {
  }
 }
 
-module.exports = { buffThumb };
+/**
+ * Function to read a file from a given path and return its content as a buffer.
+ * @param {string} relativePath - The relative path to the file.
+ * @returns {Promise<Buffer>} - A promise that resolves with the file buffer.
+ */
+const buffpath = async relativePath => {
+ try {
+  const filePath = path.join(__dirname, relativePath);
+  const fileBuffer = await fs.promises.readFile(filePath);
+  return fileBuffer;
+ } catch (error) {
+  console.error(`Error reading file from path: ${relativePath}`, error);
+  throw error;
+ }
+};
+
+module.exports = { buffThumb, buffpath };
