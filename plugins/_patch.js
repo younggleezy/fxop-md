1fb0d7f3623f88c36f21c1cd3a487054e2f3e48d
FX <danielfx011@gmail.com>
2024-09-03 12:50:58 +0100

    fix

--- plugins/_patch.js
+++ plugins/_patch.js
@@ -1,7 +1,20 @@
 const plugins = require("../lib/plugins");
-const { command, mode, clockString, tiny, formatBytes } = require("../lib");
+const { command, mode, tiny, formatBytes } = require("../lib");
 const { BOT_INFO } = require("../config");
 const os = require("os");
+
+const runtime = function (seconds) {
+ seconds = Number(seconds);
+ var d = Math.floor(seconds / (3600 * 24));
+ var h = Math.floor((seconds % (3600 * 24)) / 3600);
+ var m = Math.floor((seconds % 3600) / 60);
+ var s = Math.floor(seconds % 60);
+ var dDisplay = d > 0 ? d + (d == 1 ? " d " : " d ") : "";
+ var hDisplay = h > 0 ? h + (h == 1 ? " h " : " h ") : "";
+ var mDisplay = m > 0 ? m + (m == 1 ? " m " : " m ") : "";
+ var sDisplay = s > 0 ? s + (s == 1 ? " s" : " s") : "";
+ return dDisplay + hDisplay + mDisplay + sDisplay;
+};
 
 command(
  {
@@ -28,7 +41,7 @@ Description: ${i.desc}\`\`\``);
 ││ Date: ${date}
 ││ Time: ${time}
 ││ Plugins: ${plugins.commands.length} 
-││ Uptime: ${clockString(process.uptime())} 
+││ Uptime: ${runtime(process.uptime())} 
 ││ Ram: ${formatBytes(os.totalmem() - os.freemem())} / ${formatBytes(os.totalmem())}
 ││ Version: ${(version = require("../package.json").version)}
 │╰──────────────
