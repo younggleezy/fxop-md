ce0fa7f90c92a980b71a0dd9cff3416012e5d134
FX <danielfx011@gmail.com>
2024-09-04 19:24:00 +0100

    fix

--- plugins/search.js
+++ plugins/search.js
@@ -22,6 +22,7 @@ command(
 command(
  {
   pattern: "weather ?(.*)",
+  fromMe: mode,
   desc: "weather info",
   type: "search",
  },
@@ -32,5 +33,20 @@ command(
   const { name, timezone, sys, main, weather, visibility, wind } = data;
   const degree = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"][getFloor(wind.deg / 22.5 + 0.5) % 16];
   return await message.send(`*Name :* ${name}\n*Country :* ${sys.country}\n*Weather :* ${weather[0].description}\n*Temp :* ${getFloor(main.temp)}°\n*Feels Like :* ${getFloor(main.feels_like)}°\n*Humidity :* ${main.humidity}%\n*Visibility  :* ${visibility}m\n*Wind* : ${wind.speed}m/s ${degree}\n*Sunrise :* ${moment.utc(sys.sunrise, "X").add(timezone, "seconds").format("hh:mm a")}\n*Sunset :* ${moment.utc(sys.sunset, "X").add(timezone, "seconds").format("hh:mm a")}`);
+ }
+);
+
+command(
+ {
+  pattern: "google",
+  fromMe: true,
+  desc: "Search Google",
+  type: "search",
+ },
+ async (message, match) => {
+  if (!match) return await message.send("_need query!_");
+  await message.send("_Searching google_");
+  const results = await ScrapeSrc.google(match);
+  return message.send(results, { contextInfo: { forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363327841612745@newsletter", newsletterName: "ɢᴏᴏɢʟᴇ sᴇᴀʀᴄʜ" } } });
  }
 );
