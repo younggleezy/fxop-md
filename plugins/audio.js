7a812b347c78efc66f7388aa88225ded8a7a9f3a
FX <danielfx011@gmail.com>
2024-09-04 11:50:03 +0100

    fix

--- plugins/audio.js
+++ plugins/audio.js
@@ -1,141 +1,46 @@
 const { command } = require("../lib");
-const { editAudio } = require("../lib");
+const { editAudio } = require("../lib"); // Import the new editAudio function
+
+/**
+ * Handles the audio effect command.
+ * @param {Object} message - The message object.
+ * @param {string} effectName - The effect to apply.
+ */
 async function effectAudio(message, effectName) {
  try {
-  return await editAudio(message, effectName, message);
+  if (!message.reply_message || !message.reply_message.audio) {
+   return await message.send("_Reply Audio/Voice Note Only!_");
+  }
+  const audioBuffer = await message.client.download(message.quoted);
+  const processedAudio = await editAudio(audioBuffer, effectName);
+  await message.bot.sendMessage(
+   message.chat,
+   {
+    audio: processedAudio,
+    mimetype: "audio/mpeg",
+    ptt: /ptt|voice/.test(message.test || "") ? true : false,
+   },
+   {
+    quoted: message,
+    messageId: message.bot.messageId(),
+   }
+  );
  } catch (error) {
-  return await message.log(error);
+  await message.error(error.message);
+  console.error("Error processing audio effect:", error);
  }
 }
 
-command(
- {
-  pattern: "bass",
-  info: "adds bass in given sound",
-  type: "audio",
- },
- async (message, match) => {
-  return await effectAudio(message, match);
- }
-);
-
-command(
- {
-  pattern: "blown",
-  info: "adds blown in given sound",
-  type: "audio",
- },
- async (message, match) => {
-  return await effectAudio(message, match);
- }
-);
-
-command(
- {
-  pattern: "deep",
-  info: "adds deep in given sound",
-  type: "audio",
- },
- async (message, match) => {
-  return await effectAudio(message, match);
- }
-);
-
-command(
- {
-  pattern: "earrape",
-  info: "adds earrape in given sound",
-  type: "audio",
- },
- async (message, match) => {
-  return await effectAudio(message, match);
- }
-);
-
-command(
- {
-  pattern: "fast",
-  info: "adds fast in given sound",
-  type: "audio",
- },
- async (message, match) => {
-  return await effectAudio(message, match);
- }
-);
-
-command(
- {
-  pattern: "fat",
-  info: "adds fat in given sound",
-  type: "audio",
- },
- async (message, match) => {
-  return await effectAudio(message, match);
- }
-);
-
-command(
- {
-  pattern: "nightcore",
-  info: "adds nightcore in given sound",
-  type: "audio",
- },
- async (message, match) => {
-  return await effectAudio(message, match);
- }
-);
-
-command(
- {
-  pattern: "reverse",
-  info: "adds reverse in given sound",
-  type: "audio",
- },
- async (message, match) => {
-  return await effectAudio(message, match);
- }
-);
-
-command(
- {
-  pattern: "robot",
-  info: "adds robot in given sound",
-  type: "audio",
- },
- async (message, match) => {
-  return await effectAudio(message, match);
- }
-);
-
-command(
- {
-  pattern: "slow",
-  info: "adds slow in given sound",
-  type: "audio",
- },
- async (message, match) => {
-  return await effectAudio(message, match);
- }
-);
-
-command(
- {
-  pattern: "smooth",
-  info: "adds smooth in given sound",
-  type: "audio",
- },
- async (message, match) => {
-  return await effectAudio(message, match);
- }
-);
-
-command(
- {
-  pattern: "tupai",
-  info: "adds tupai in given sound",
-  type: "audio",
- },
- async (message, match) => {
-  return await effectAudio(message, match);
- }
-);
+const effects = ["bass", "blown", "deep", "earrape", "fast", "fat", "nightcore", "reverse", "robot", "slow", "smooth", "tupai"];
+effects.forEach(effect => {
+ command(
+  {
+   pattern: effect,
+   info: `Adds ${effect} effect to the given sound`,
+   type: "audio",
+  },
+  async (message, match) => {
+   return await effectAudio(message, effect);
+  }
+ );
+});
