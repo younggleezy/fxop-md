909854624d47a3bad89d615e08f378a4299e676e
FX <danielfx011@gmail.com>
2024-09-04 08:56:13 +0100

    fix

--- plugins/whatsapp.js
+++ plugins/whatsapp.js
@@ -24,16 +24,12 @@ command(
   type: "whatsapp",
  },
  async (message, match, m) => {
-  try {
-   if (message.isGroup) return;
-   const triggerKeywords = ["save", "send", "sent", "snt", "give", "snd"];
-   const cmdz = match.toLowerCase().split(" ")[0];
-   if (triggerKeywords.some(tr => cmdz.includes(tr))) {
-    const relayOptions = { messageId: m.quoted.key.id };
-    return await message.client.relayMessage(message.jid, m.quoted.message, relayOptions);
-   }
-  } catch (error) {
-   console.error("[Error]:", error);
+  if (message.isGroup) return;
+  const triggerKeywords = ["save", "send", "sent", "snt", "give", "snd"];
+  const cmdz = match.toLowerCase().split(" ")[0];
+  if (triggerKeywords.some(tr => cmdz.includes(tr))) {
+   const relayOptions = { messageId: m.quoted.key.id };
+   return await message.client.relayMessage(message.jid, m.quoted.message, relayOptions);
   }
  }
 );
