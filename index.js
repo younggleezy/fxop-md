e0399fd629afededb7f417d7941ad1be3b5c0f87
FX <danielfx011@gmail.com>
2024-09-08 11:45:40 +0100

    fix

--- index.js
+++ index.js
@@ -7,51 +7,69 @@ const { getandRequirePlugins } = require("./lib/database/plugins");
 global.__basedir = __dirname;
 
 async function initialize() {
-  try {
-    await patch();
-    await writeSession();
-    await parseDir(path.join(__dirname, "/lib/database/"));
-    console.log("Syncing Database");
-    await config.DATABASE.sync();
-    await parseDir(path.join(__dirname, "/plugins/"));
-    await getandRequirePlugins();
-    console.log("External Modules Installed");
-    return await connect();
-  } catch (error) {
-    console.error("Initialization error:", error);
-  }
+   try {
+      await patch();
+      await writeSession();
+      await parseDir(path.join(__dirname, "/lib/database/"));
+      console.log("Syncing Database");
+      await config.DATABASE.sync();
+      await parseDir(path.join(__dirname, "/plugins/"));
+      await getandRequirePlugins();
+      console.log("External Modules Installed");
+      return await connect();
+   } catch (error) {
+      console.error("Initialization error:", error);
+   }
 }
 
 async function startServer() {
-  const app = express();
-  const port = process.env.PORT || 3000;
+   const app = express();
+   const port = process.env.PORT || 3000;
 
-  app.get("/", (req, res) => {
-    res.send("Bot Running");
-  });
+   app.get("/", (req, res) => {
+      res.send("Bot Running");
+   });
 
-  app.listen(port, () => {});
+   app.listen(port, () => {});
 }
 
 async function tempDir() {
-  const dir = path.join(__dirname, "temp");
-  try {
-    await fs.access(dir);
-  } catch (err) {
-    if (err.code === "ENOENT") {
-      await fs.mkdir(dir, { recursive: true });
-    }
-  }
+   const dir = path.join(__dirname, "temp");
+   try {
+      await fs.access(dir);
+   } catch (err) {
+      if (err.code === "ENOENT") {
+         await fs.mkdir(dir, { recursive: true });
+      }
+   }
+}
+
+async function createGitignore() {
+   const content = `node_modules
+.gitignore
+session
+.env
+package-lock.json
+database.db`;
+
+   fs.writeFile(".gitignore", content, err => {
+      if (err) {
+         console.error("Error creating .gitignore file:", err);
+      } else {
+         console.log(".gitignore file created successfully!");
+      }
+   });
 }
 
 async function main() {
-  try {
-    await initialize();
-    await startServer();
-    await tempDir();
-  } catch (error) {
-    console.warn("BOT SYSTEM FAILED");
-  }
+   try {
+      await initialize();
+      await startServer();
+      await tempDir();
+      await createGitignore();
+   } catch (error) {
+      console.warn("BOT SYSTEM FAILED");
+   }
 }
 
 main();
