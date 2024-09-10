ae4c2fb59c4a7cb35e73eff3487849d8aa6edb58
FX <danielfx011@gmail.com>
2024-09-10 18:11:23 +0100

    adjust

--- config.js
+++ config.js
@@ -1,38 +1,46 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
+
 const toBool = x => x === "true";
 const DATABASE_URL = process.env.DATABASE_URL || "./database.db";
+
 module.exports = {
+   // Boolean Values (converted from Strings)
+   LOGS: toBool(process.env.LOGS) || false,
+   ANTILINK: toBool(process.env.ANTI_LINK) || false,
+   HEROKU: toBool(process.env.HEROKU) || false,
+   AUTO_READ: toBool(process.env.AUTO_READ) || false,
+   AUTO_STATUS_READ: toBool(process.env.AUTO_STATUS_READ) || false,
+   DELETED_LOG: toBool(process.env.DELETED_LOG) || false,
+   STATUS_SAVER: toBool(process.env.STATUS_SAVER) || true,
+
+   // String Values
    SESSION_ID: process.env.SESSION_ID || "",
    HANDLERS: process.env.HANDLER === "false" || process.env.HANDLER === "null" ? "^" : "[.]",
-   BOT_INFO: process.env.BOT_INFO || "ᴀsᴛʀᴏ;ғxᴏᴘ-ᴍᴅ", //YourName;BotName;Image/VideoLink
+   BOT_INFO: process.env.BOT_INFO || "ᴀsᴛʀᴏ;ғxᴏᴘ-ᴍᴅ", // YourName;BotName;Image/VideoLink
    SUDO: process.env.SUDO || "91234567899",
-   ANTICALL: process.env.ANTICALL || false,
    PRESENCE: process.env.PRESENCE || "available", // "unavailable", "available", "composing", "recording", "paused"
    CAPTION: process.env.CAPTION || "© ғxᴏᴘ ᴍᴅ",
    TIME_ZONE: process.env.TIME_ZONE || "Africa/Lagos",
-   LOGS: toBool(process.env.LOGS) || true,
    ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
    BRANCH: "master",
-   WARN_COUNT: 3,
    AUTHOR: process.env.AUTHOR || "ғxᴏᴘ-ᴍᴅ",
    ANTIWORD: process.env.ANTIWORD || "badword1,badword2,badword3",
    PACKNAME: process.env.PACKNAME || "ғxᴏᴘ-ᴍᴅ",
    WELCOME_MSG: process.env.WELCOME_MSG || "Hi @user Welcome to @gname",
    GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
-   ANTILINK: toBool(process.env.ANTI_LINK) || false,
-   HEROKU: toBool(process.env.HEROKU) || false,
-   AUTO_READ: toBool(process.env.AUTO_READ) || false,
    RMBG_KEY: process.env.RMBG_KEY || "",
-   AUTO_STATUS_READ: toBool(process.env.AUTO_STATUS_READ) || false,
    WORK_TYPE: process.env.WORK_TYPE || "private",
-   DELETED_LOG: toBool(process.env.DELETED_LOG) || false,
    DELETED_LOG_CHAT: process.env.DELETED_LOG_CHAT || false,
    REMOVEBG: process.env.REMOVEBG || "",
-   DATABASE_URL: DATABASE_URL,
-   STATUS_SAVER: toBool(process.env.STATUS_SAVER) || true,
    HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
    HEROKU_API_KEY: process.env.HEROKU_API_KEY,
+
+   // Number Values
+   WARN_COUNT: 3,
+
+   // Database Configuration
+   DATABASE_URL: DATABASE_URL,
    DATABASE:
       DATABASE_URL === "./database.db"
          ? new Sequelize({
